1aed5fa7c4dff945d5695501d175d2fef1ade9e8
Thomas Shone <xsist10@gmail.com>
2015-03-11 08:09:34 +0100

    Added tests I forgot to commit last push

---
+++ tests/Repository/Repository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Consolidate\Ticket\Tests\Repository;
+
+use \PHPUnit_Framework_TestCase as PHPUnit_Framework_TestCase;
+
+use Consolidate\Ticket\Ticket;
+use Consolidate\Ticket\Repository\Repository;
+use Consolidate\Ticket\Repository\Storage\Store;
+
+class RepositoryTest extends PHPUnit_Framework_TestCase
+{
+    public function testLoadAndPersist() {
+        $repository = new Repository();
+        $repository->setStorage(new FakeStore());
+        
+        $ticket = $repository->load(1);
+        $this->assertEquals(1, $ticket->getId());
+        $this->assertTrue($repository->persist($ticket));
+    }
+
+    public function testMissingStore() {
+        $this->setExpectedException('Exception', 'No storage engine specified.');
+
+        $repository = new Repository();
+        $repository->load(1);
+    }
+}
+
+
+class FakeStore implements Store {
+    public function load($id)
+    {
+        return new Ticket($id);
+    }
+
+    public function persist(Ticket $ticket)
+    {
+        return true;
+    }
+}
